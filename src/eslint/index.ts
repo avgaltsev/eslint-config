b72c78b48f4c55b12852e8f99df29a7a1b1f3e40
Anton Galtsev <avgaltsev@gmail.com>
2024-07-06 00:37:58 -0700

    Temporarily add all old rules to check what's deprecated

--- src/eslint/index.ts
+++ src/eslint/index.ts
@@ -1,10 +1,16 @@
 import type { FlatConfig } from "@typescript-eslint/utils/dist/ts-eslint";
 
+import { rules as oldRules } from "./rules_old.js";
 import { rules as problemRules } from "./rules_problem.js";
 import { rules as suggestionRules } from "./rules_suggestion.js";
 import { rules as layoutRules } from "./rules_layout.js";
 
 const configs: FlatConfig.Config[] = [
+	{
+		name: "eslint/old",
+		rules: oldRules,
+	},
+
 	{
 		name: "eslint/problems",
 		rules: problemRules,
--- src/typescriptEslint/index.ts
+++ src/typescriptEslint/index.ts
@@ -2,10 +2,16 @@ import type { FlatConfig } from "@typescript-eslint/utils/dist/ts-eslint";
 
 import { plugin } from "typescript-eslint";
 
+import { rules as oldRules } from "./rules_old.js";
 import { rules as problemRules } from "./rules_problem.js";
 import { rules as suggestionRules } from "./rules_suggestion.js";
 
 const baseConfigs: FlatConfig.Config[] = [
+	{
+		name: "typescript-eslint/old",
+		rules: oldRules,
+	},
+
 	{
 		name: "typescript-eslint/problem",
 		rules: problemRules,
